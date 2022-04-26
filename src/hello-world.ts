20252f277419fff63e9f135d897e865fc1351ff4
Jon McMillan <mcmillan.jon@gmail.com>
2022-04-26 14:17:02 +0200

    Add data to the pages created by API
    Added
    - Emoji
    - Child block
    
    Included some refactoring

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,28 +1,79 @@
 import { Client } from "@notionhq/client";
 
-if (!process.env.NOTION_TOKEN) {
-  console.log(
-    "❌ No NOTION_TOKEN found. Please set it in your environment variables."
+// They don't export their type names it appears
+
+type SearchResults = Awaited<ReturnType<Client["search"]>>["results"];
+type Database = Awaited<ReturnType<Client["databases"]["retrieve"]>>;
+type DatabaseProperty = Database["properties"][string];
+
+/**
+ * Databases, pages, and blocks are all nodes.
+ */
+type Node = {
+  id: string;
+};
+
+const initClient = (): Client => {
+  if (!process.env.NOTION_TOKEN) {
+    throw new Error(
+      "No NOTION_TOKEN found. Please set it in your environment variables."
+    );
+  }
+
+  return new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+};
+
+/**
+ * Find the title property.  Just an example of how to find properties by type.
+ */
+const getTitleProperty = (db: Database) => {
+  const titleProp = Object.values(db.properties).find(
+    (prop) => prop.type === "title"
   );
-  process.exit(1);
-}
 
-// Initializing a client
-const notion = new Client({
-  auth: process.env.NOTION_TOKEN,
-});
+  if (!titleProp) {
+    throw new Error("No title property found");
+  }
 
-const createSimplePage = async (dbId: string) =>
+  return titleProp;
+};
+
+/**
+ * Helps typescript do type discrimination with the search results
+ */
+const getDatabaseResult = (results: SearchResults): Database => {
+  for (const result of results) {
+    if (result.object === "database") {
+      return result;
+    }
+  }
+
+  throw new Error(
+    "No database found. A database must be shared with the integration to be visible."
+  );
+};
+
+const createSimplePage = async (
+  notion: Client,
+  dbId: string,
+  titleProp: DatabaseProperty
+) =>
   notion.pages.create({
     parent: {
       database_id: dbId,
     },
+    icon: {
+      type: "emoji",
+      emoji: "✅",
+    },
     properties: {
-      Name: {
+      [titleProp.name]: {
         title: [
           {
             text: {
-              content: "Hello from the API",
+              content: "Hello API World",
             },
           },
         ],
@@ -30,27 +81,49 @@ const createSimplePage = async (dbId: string) =>
     },
   });
 
+const writeChildBlock = async (notion: Client, page: Node) =>
+  notion.blocks.children.append({
+    block_id: page.id,
+    children: [
+      {
+        type: "paragraph",
+        paragraph: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: "API body...", link: null },
+            },
+          ],
+        },
+      },
+    ],
+  });
+
 const main = async () => {
-  const { results: databases } = await notion.search({
+  const notion = initClient();
+
+  const { results } = await notion.search({
     filter: { property: "object", value: "database" },
   });
 
-  if (databases.length === 0) {
-    console.log(
-      "❌ No database found. A database must be shared with the integration to be visible."
-    );
-    throw new Error("No database found");
-  }
+  const db = getDatabaseResult(results);
+  const titleProp = getTitleProperty(db);
 
-  const databaseId = databases[0].id;
-  await createSimplePage(databaseId);
+  const page = await createSimplePage(notion, db.id, titleProp);
   console.log("✅ Page created");
+
+  await writeChildBlock(notion, page);
+  console.log("✅ Block added");
 };
 
 main()
   .then(() => {
     process.exit(0);
   })
-  .catch(() => {
+  .catch((err) => {
+    if (err instanceof Error) {
+      console.log("❌ " + err.message);
+    }
+
     process.exit(1);
   });
